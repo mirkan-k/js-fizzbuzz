b0a4de4cedff6671132056728b5c0801a71bbc81
mirkan-k <mirkilinc@gmail.com>
2022-05-23 10:38:00 +0100

    specComplete

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,21 @@
 // TODO: Write your function in this file
-
+function createFizzBuzzArray(lower, upper) {
+    const fizzBuzzArray = []
+  
+    for (let i = lower; i <= upper; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            fizzBuzzArray.push('FizzBuzz')
+        } else if (i % 3 === 0) {
+            fizzBuzzArray.push('Fizz')
+        } else if (i % 5 === 0) {
+            fizzBuzzArray.push('Buzz')
+        } else {
+            fizzBuzzArray.push(i)
+        }
+    }
+    
+    return fizzBuzzArray
+  }
+  console.log(createFizzBuzzArray(1, 50))
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = createFizzBuzzArray
